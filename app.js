3702c11723c0c670706abc73fd90b9b03fc4618c
juzzmirha <crizzenn@gmail.com>
2024-02-19 18:07:05 +0600

    Hot

---
+++ app.js
@@ -0,0 +1,25 @@
+const no_btn = document.getElementById('no');
+var yes_btn = document.getElementById('yes');
+var modal = document.getElementById('modal');
+var close_btn = document.getElementsByClassName('close')[0];
+
+no_btn.addEventListener('click', function() {
+    function moveButton() {
+        var randomX = Math.floor(Math.random() * (window.innerWidth - no_btn.offsetWidth));
+        var randomY = Math.floor(Math.random() * (window.innerHeight - no_btn.offsetHeight));
+
+        no_btn.style.position = 'absolute';
+        no_btn.style.left = randomX + 'px';
+        no_btn.style.top = randomY + 'px';
+    }
+    moveButton();
+});
+
+
+yes_btn.onclick = function(){
+    modal.style.display = "block";
+}
+
+close_btn.onclick = function() {
+    modal.style.display = "none";
+}
